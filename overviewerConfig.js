8aed2e23c61637cfe8897bfe2fbb397cbbb2d4f9
Robert Washbourne <rawashbourne@gmail.com>
2019-05-27 16:49:30 -0500

    new map

---
+++ overviewerConfig.js
@@ -0,0 +1,115 @@
+var overviewerConfig = {
+    "worlds": [
+        "mc.raw.sh - overworld"
+    ], 
+    "tilesets": [
+        {
+            "spawn": [
+                143, 
+                73, 
+                192
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1558988102, 
+            "name": "overworld", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 6, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 4, 
+            "world": "mc.raw.sh - overworld", 
+            "maxZoom": 6, 
+            "path": "top", 
+            "showlocationmarker": true
+        }, 
+        {
+            "spawn": [
+                143, 
+                73, 
+                192
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1558988102, 
+            "name": "night", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 6, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 4, 
+            "world": "mc.raw.sh - overworld", 
+            "maxZoom": 6, 
+            "path": "night", 
+            "showlocationmarker": true
+        }, 
+        {
+            "spawn": [
+                143, 
+                73, 
+                192
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1558988102, 
+            "name": "caves", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 6, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 4, 
+            "world": "mc.raw.sh - overworld", 
+            "maxZoom": 6, 
+            "path": "cave", 
+            "showlocationmarker": true
+        }
+    ], 
+    "CONST": {
+        "mapDivId": "mcmap", 
+        "UPPERLEFT": 0, 
+        "tileSize": 384, 
+        "UPPERRIGHT": 1, 
+        "image": {
+            "queryMarker": "icons/marker_location.png", 
+            "spawnMarker": "icons/marker_home.png", 
+            "spawnMarker2x": "icons/marker_home_2x.png", 
+            "signMarker": "signpost_icon.png", 
+            "queryMarker2x": "icons/marker_location_2x.png", 
+            "compass": {
+                "0": "compass_upper-left.png", 
+                "1": "compass_upper-right.png", 
+                "2": "compass_lower-right.png", 
+                "3": "compass_lower-left.png"
+            }, 
+            "defaultMarker": "signpost.png", 
+            "bedMarker": "bed.png"
+        }, 
+        "LOWERRIGHT": 2, 
+        "LOWERLEFT": 3
+    }, 
+    "map": {
+        "debug": true, 
+        "north_direction": "lower-left", 
+        "controls": {
+            "spawn": true, 
+            "coordsBox": true, 
+            "zoom": true, 
+            "mapType": true, 
+            "compass": true, 
+            "overlays": true, 
+            "pan": true
+        }, 
+        "center": [
+            -314, 
+            67, 
+            94
+        ], 
+        "cacheTag": "1558993570"
+    }
+};
